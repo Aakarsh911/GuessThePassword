1076058a78b4d186c613a532c8f4f72e2bc0b2c7
Aakarsh Kaushal <aakarsh99@icloud.com>
2024-01-14 04:03:23 -0500

    Using .env

--- backend/server.js
+++ backend/server.js
@@ -12,33 +12,7 @@ app.listen(port, () => console.log(`Server running on port ${port}`));
 app.use(cors());
 app.use(express.json());
 
-const apiKey = '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';
-
-app.get('/generate/:show', async (req, res) => {
-    try {
-        const { show } = req.params;
-        prompt = `This is a number ${show}. Change any one digit of this number and return it. Just return the number and nothing else`;
-        const requestData = {
-            model: 'gpt-3.5-turbo-1106',
-            messages: [
-                { role: 'system', content: prompt },
-                { role: 'user', content: prompt },
-            ],
-        };
-        const response = await axios.post('https://api.openai.com/v1/chat/completions', requestData, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${apiKey}`,
-            },
-        });
-        const answer = response.data.choices[0].message.content;
-        console.log(answer);
-    }
-    catch (error) {
-        console.error('Error:', error.response ? error.response.data : error.message);
-        res.status(500).send('Internal Server Error');
-    }
-});
+const apiKey = process.env.OPENAI_API_KEY;
 
 app.post('/ask/:guess/:show', async (req, res) => {
     const { guess } = req.params;
